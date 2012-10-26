9d3f82531cb7b562004210df97a5d991ffb214f2
Yves Brissaud <ybrissaud@tea-ebook.com>
2012-10-26 15:58:47 +0200

    Add: generate a secret if not exists
    Closes #12

--- config/initializers/check_session_secret.rb
+++ config/initializers/check_session_secret.rb
@@ -1,31 +1,27 @@
 # encoding: utf-8
 
 # Copyright (C) 2012  TEA, the ebook alternative <http://www.tea-ebook.com/>
-# 
+#
 # This file is part of TeaBook Open Reader
-# 
+#
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, version 3.0 of the License.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# 
-# An additional permission has been granted as a special exception 
-# to the GNU General Public Licence. 
-# You should have received a copy of this exception. If not, see 
+#
+# An additional permission has been granted as a special exception
+# to the GNU General Public Licence.
+# You should have received a copy of this exception. If not, see
 # <https://github.com/TEA-ebook/teabook-open-reader/blob/master/GPL-3-EXCEPTION>.
 
-
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Tea::Application.config.secret_token = 'your secret'
+if !File.exists?( Rails.root.join('config', 'initializers', 'secret_token.rb'))
+  `bundle exec rake generate:secret_token`
+  require  Rails.root.join('config', 'initializers', 'secret_token.rb')
+end
